afa647f6c8615c8cce6634c5cd9a372ab0a78a10
Lukas <lvozenilek@gmail.com>
2014-09-21 20:22:09 -0700

    Added beeping, LCD works, finished input function, and various asthetic improvements.

--- arduino/os/os.ino
+++ arduino/os/os.ino
@@ -2,8 +2,7 @@
 #include <Keypad.h>
 
 
-//Arduino pins to LCD pins
-//52-15, 53-4, 51-5, 49-6, 47-7, 45-8, 43-9, 41-*
+//LCD setup
 LiquidCrystal lcd(53, 51, 49, 47, 45, 43, 41);
 int backlight = 52;
 
@@ -21,6 +20,7 @@ byte colPins[cols] = {22, 24, 26};
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 
+//Other variables
 char result = 0;
 
 //Menu data
@@ -68,13 +68,6 @@ char menu1d_3[10] = "3: Custom";
 char menu1d_4[7] = "4: Off";
 char* menu1d[4];
 
-//Menu 1d3 - Custom
-char menu1d3_1[21] = "Heading, Pitch, Roll";
-char menu1d3_2[1] = "";
-char menu1d3_3[1] = "";
-char menu1d3_4[1] = "";
-char* menu1d3[4];
-
 //Menu 2 - Scripts
 char menu2_1[20] = "";
 char menu2_2[20] = "";
@@ -86,9 +79,8 @@ char* menu2[4];
 char menu3_1[17] = "1: Telemachus IP";
 char menu3_2[18] = "2: Multiplayer IP";
 char menu3_3[21] = "3: Start Multiplayer";
-char menu3_4[15] = "4: ";
+char menu3_4[15] = "4: Wifi Preset";
 char* menu3[4];
-
 
 //General I/O pins
 int b1 = 30;
@@ -102,14 +94,6 @@ int led2 = 41;
 int led3 = 42;
 int led4 = 43;
 int led5 = 44;
-int led6 = 45;
-
-int led1_tog = 0;
-int led2_tog = 0;
-int led3_tog = 0;
-int led4_tog = 0;
-int led5_tog = 0;
-int led6_tog = 0;
 
 int abort_pin = 35;
 
@@ -119,6 +103,8 @@ int map_pin = 37;
 int map_last = 0;
 
 int buzzer = 2;
+int beep1 = 510;
+int beep1len = 100;
 
 int throttle_pin = 0;
 float throttle = 0;
@@ -126,32 +112,63 @@ float last_throttle = 0;
 
 
 
-//Code stuff
-char code1 = '1';
-char code2 = '2';
-char code3 = '3';
-char code4 = '4';
-char enter[4];
-int e_count = 0;
-
-
-void write_menu(char* input_menu[]) {
+//Write menus
+void write_menu(char* input_menu[], int beep) {
   lcd.clear();
   for (int index = 0; index < 4; index++) {
     lcd.setCursor(0,index);
-    //Serial.println(input_menu[index]);
-    lcd.print(input_menu[index]);  
+    lcd.print(input_menu[index]);
   }
-  Serial.print("\n");
+  if (beep == 1) {
+    tone(buzzer,beep1,beep1len);
+  }
+}
+
+//Double tone acknowledge
+void ak_tone() {
+  tone(buzzer,600,100);
+  delay(200);
+  tone(buzzer,600,100);
+  delay(200);
+}
+
+//Return user inputted number from keypad
+char* input(char prompt[], char seperator, int hidden = 0, int beep = 1) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(prompt);
+  char output[21];
+  int count = 0;
+  while (1) {
+    result = keypad.getKey();
+    if (result != NO_KEY && result != '#') {
+      if (result == '*') {
+        if (seperator != '0') {
+          result = seperator;
+        }  
+      }
+      tone(buzzer,beep1,beep1len);
+      lcd.setCursor(count,2);
+      if (hidden == 0) {
+        lcd.print(result);
+      } else {
+        lcd.print('*');
+      }
+      output[count] = result;
+      count += 1;
+    } else if (result == '#') {
+      break;
+    }
+  }
+  if (beep == 1) {
+    ak_tone();
+  }
+  output[count] = '\0';
+  return output;
 }
 
 
-
-boolean float_compare(float f1, float f2){
-  return ( (int)(f1 *100)) == ((int)(f2 * 100) );
-}
-
-//Check everything on panel and send changes via Serial1
+//Check everything on panel and send changes via Serial
 void check_panel() {
     int change = 0; 
     if (digitalRead(b1) == 1) {
@@ -159,48 +176,57 @@ void check_panel() {
         delay(0.1);  
       }
       change = 1;
-      Serial1.print("[C1]");
+      Serial.print("[C1]");
+      
     } else if (digitalRead(b2) == 1) {
       while (digitalRead(b2) == 1) {
         delay(0.1);  
       }
       change = 1;
-      Serial1.print("[C2]");      
+      Serial.print("[C2]");
+      
     } else if (digitalRead(b3) == 1) {
       while (digitalRead(b3) == 1) {
         delay(0.1);  
       }
       change = 1;
-      Serial1.print("[C3]");      
+      Serial.print("[C3]");
+      
     } else if (digitalRead(b4) == 1) {
       while (digitalRead(b4) == 1) {
         delay(0.1);  
       }
       change = 1;
-      Serial1.print("[C4]");      
+      Serial.print("[C4]");
+      
     } else if (digitalRead(b5) == 1) {
       while (digitalRead(b5) == 1) {
         delay(0.1);  
       }
       change = 1;
-      Serial1.print("[C5]");      
+      Serial.print("[C5]");
+      
     } else if (digitalRead(release_pin) == 1) {
       while (digitalRead(release_pin) == 1) {
         delay(0.1);  
       }
-      Serial1.print("[C6]");      
+      Serial.print("[C6]");
+      
     } else if (digitalRead(abort_pin) == 1) {
       while (digitalRead(abort_pin) == 1) {
         delay(0.1);
       }
-      Serial1.print("[C7]");      
+      Serial.print("[C7]");
+      
     } else if (digitalRead(map_pin) == 1 && map_last == 0) {
-      Serial1.print("[C8]");
+      Serial.print("[C8]");
       map_last = 1;
+      
     } else if (digitalRead(map_pin) == 0 && map_last == 1) {
-      Serial1.print("[C9]");
+      Serial.print("[C9]");
       map_last = 0;  
     }
+    
     throttle = map(analogRead(0), 0, 1023, 0, 100);
     throttle = throttle * -1;
     throttle = throttle + 100;
@@ -213,11 +239,10 @@ void check_panel() {
     */
     if (throttle != last_throttle && throttle != last_throttle - 1 && throttle != last_throttle + 1) {
       last_throttle = throttle;
-      Serial1.print("[T");
-      //Serial1.print(((float)((int)(throttle * 1000))) / 1000);
-      Serial1.print(throttle/100);
-      Serial1.print("]");
-      Serial.println(throttle);
+      Serial.print("[T");
+      //Serial.print(((float)((int)(throttle * 1000))) / 1000);
+      Serial.print(throttle/100);
+      Serial.print("]");
     }
     if (change == 1) {
       update_leds();  
@@ -226,12 +251,13 @@ void check_panel() {
 
 //Update LEDs above control buttons with data from Telemachus
 void update_leds() {
-  Serial1.print("[R2]");
+  Serial.print("[R2]");
   while (1) {
-    if (Serial1.available() >= 5) {
+    //Wait for 5 bytes
+    if (Serial.available() >= 5) {
       //process data
-      char output[5] = {};
-      Serial1.readBytes(output,5);
+      char output[5];
+      Serial.readBytes(output,5);
       digitalWrite(led1, output[0]);
       digitalWrite(led2, output[1]);
       digitalWrite(led3, output[2]);
@@ -244,37 +270,14 @@ void update_leds() {
 }
 
 
-int charToInt(char ch) {
-  if (ch == '1') {
-    return 1;  
-  } else if (ch == '2') {
-    return 2; 
-  }  else if (ch == '3') {
-    return 3; 
-  } else if (ch == '4') {
-    return 4; 
-  } else if (ch == '5') {
-    return 5; 
-  } else if (ch == '6') {
-    return 6; 
-  } else if (ch == '7') {
-    return 7; 
-  } else if (ch == '8') {
-    return 8; 
-  } else if (ch == '9') {
-    return 9; 
-  } else if (ch == '*') {
-    return 10; 
-  } else if (ch == '#') {
-    return 11; 
-  }
-}
+
+
 
 
 void setup() {
-  Serial1.begin(9600);
   Serial.begin(9600);
-    
+  Serial.begin(9600);
+  
   //LCD setup
   pinMode(backlight, OUTPUT);
   digitalWrite(backlight, HIGH);
@@ -312,76 +315,112 @@ void setup() {
   menu1d[2] = menu1d_3;
   menu1d[3] = menu1d_4;
   
-  menu1d3[0] = menu1d3_1;
-  menu1d3[1] = menu1d3_2;
-  menu1d3[2] = menu1d3_3;
-  menu1d3[3] = menu1d3_4;
-  
   menu3[0] = menu3_1;
   menu3[1] = menu3_2;
   menu3[2] = menu3_3;
   menu3[3] = menu3_4;
   
   
-  /*
-  //Enter code to get access to board
+  
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Please enter code");
-  Serial.print("Setup complete, enter code\n");
+  lcd.print("Welcome to the");
+  lcd.setCursor(0,1);
+  lcd.print("Kerbal Space Program");
+  lcd.setCursor(0,2);
+  lcd.print("control panel.");
+  
+  delay(4000);
+  //Enter code to get access to board
   while (1) {
-    result = keypad.getKey();
-    if (result != NO_KEY) {
-      enter[e_count] = result;
-      lcd.setCursor(e_count,3);
-      lcd.print("*");
-      e_count += 1;
+    char *output = input("Please enter code",'0',1,0);
+ 
+    
+    if (output[0] == '1' && output[1] == '2' && output[2] == '3' && output[3] == '4') {
+      break;
+    } else {
+      lcd.clear();
+      lcd.setCursor(0,2);
+      lcd.print("Code incorrect");
+      tone(buzzer,400,100);
+      delay(200);
+      tone(buzzer,400,100);
+      delay(2000); 
     }
-    if (e_count == 4) {
-      if (enter[0] == code1 && enter[1] == code2 && enter[2] == code3 && enter[3] == code4) {
-        break;
-      } else {
-        lcd.clear();
-        lcd.setCursor(0,3);
-        lcd.print("Code incorrect");
-        delay(2000);
+  }
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Code correct");
+  ak_tone();
+  delay(1000);
+  lcd.setCursor(0,2);
+  lcd.print("Welcome");
+  delay(1000);
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Waiting for RasPi");
+  lcd.setCursor(0,1);
+  lcd.print("      to boot      ");
+  int count = 0;
+  int loadcount = 0;
+  while (1) {
+    if (Serial.available() > 0 || 1 == 1) {
+      break;  
+    }
+    
+    
+    if (count == 10) {
+      count = 0;
+      if (loadcount == 5) {
+        loadcount = 0;
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("Please enter code");
-        e_count = 0;
+        lcd.print(" Waiting for RasPi");
+        lcd.setCursor(0,1);
+        lcd.print("      to boot      ");  
+      } else {
+        lcd.setCursor(5+(loadcount*2),3);
+        lcd.print("*");
+        loadcount += 1;
       }
+      
     }
+    count += 1;
     delay(50);  
-  } 
+  }
   
-  */
   
 }
 
 
 
 void loop() {
-  write_menu(menu0);
+  //Main menu
+  write_menu(menu0, 0);
   while (1) {
-    
     result = keypad.getKey();
     if (result == '1') {
-      write_menu(menu1);
+      //Manual control
+      write_menu(menu1, 1);
       while (1) {
         check_panel();
         result = keypad.getKey();
         if (result == '*') {
-          write_menu(menu0);
+          write_menu(menu0, 1);
           break;
         } else if (result == '1') {
-          write_menu(menu1a);
+          //Engines
+          write_menu(menu1a,1);
           while (1) {
             check_panel();
             result = keypad.getKey();
             if (result == '*') {
-              write_menu(menu1);
+              write_menu(menu1,1);
               break;    
             } else if (result != NO_KEY and result != '#') {
+              tone(buzzer,beep1,beep1len);
               lcd.setCursor(10,3);
               lcd.print(result);
               int b = 0;
@@ -390,8 +429,9 @@ void loop() {
                 char result2 = keypad.getKey();
                 if (result2 == '*') {
                   b = 1;
-                  break;  
+                  break;
                 } else if (result2 == '#') {
+                  ak_tone();
                   break;  
                 }
                 delay(50);
@@ -399,23 +439,25 @@ void loop() {
               if (b == 0) {
                 int num = result - '0';
                 int ag = num + 10;
-                Serial1.print("[C");
-                Serial1.print(ag);
-                Serial1.print("]");
+                Serial.print("[C");
+                Serial.print(ag);
+                Serial.print("]");
               }
-              write_menu(menu1);
+              write_menu(menu1,0);
               break; 
             }
           }
         } else if (result == '2') {
-          write_menu(menu1b);
+          //Decouplers
+          write_menu(menu1b,1);
           while (1) {
             check_panel();
             result = keypad.getKey();
             if (result == '*') {
-              write_menu(menu1);
+              write_menu(menu1,1);
               break;
             } else if (result != NO_KEY and result != '#') {
+              tone(buzzer,beep1,beep1len);
               lcd.setCursor(10,3);
               lcd.print(result);
               int b = 0;
@@ -426,6 +468,7 @@ void loop() {
                   b = 1;
                   break;  
                 } else if (result2 == '#') {
+                  ak_tone();
                   break;  
                 }
                 delay(50);
@@ -433,23 +476,25 @@ void loop() {
               if (b == 0) {
                 int num = result - '0';
                 int ag = num + 20;
-                Serial1.print("[C");
-                Serial1.print(ag);
-                Serial1.print("]");
+                Serial.print("[C");
+                Serial.print(ag);
+                Serial.print("]");
               }
-              write_menu(menu1);
+              write_menu(menu1,0);
               break;
             }
           }
         } else if (result == '3') {
-          write_menu(menu1c);
+          //Action Groups
+          write_menu(menu1c,1);
           while (1) {
             check_panel();
             result = keypad.getKey();
             if (result == '*') {
-              write_menu(menu1);
+              write_menu(menu1,1);
               break;    
             } else if (result != NO_KEY and result != '#') {
+              tone(buzzer,beep1,beep1len);
               lcd.setCursor(10,3);
               lcd.print(result);
               int b = 0;
@@ -460,82 +505,165 @@ void loop() {
                   b = 1;
                   break;  
                 } else if (result2 == '#') {
+                  ak_tone();
                   break;  
                 }
                 delay(50);
               }
               if (b == 0) {
                 int num = result - '0';
-                Serial1.print("[C");
-                Serial1.print(num);
-                Serial1.print("]");
+                Serial.print("[C");
+                Serial.print(num);
+                Serial.print("]");
               }
-              write_menu(menu1);
+              write_menu(menu1,0);
               break; 
             }
           }    
         } else if (result == '4') {
-          write_menu(menu1d);
+          //Attutude
+          write_menu(menu1d,1);
           while (1) {
             check_panel();
             result = keypad.getKey();
             if (result == '*') {
-              write_menu(menu1);
+              write_menu(menu1,1);
               break;    
             } else if (result == '1') {
               //Pro
+              Serial.print("[A1]");
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("Prograde set");
+              ak_tone();
+              delay(1000);
+              write_menu(menu1,0);
               break;
             } else if (result == '2') {
               //Retro
-              break;  
+              Serial.print("[A2]");
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("Retrograde set");
+              ak_tone();
+              delay(1000);
+              write_menu(menu1,0);
+              break;
             } else if (result == '3') {
-              write_menu(menu1d3);
-              while (1) {
-                check_panel();
-                result = keypad.getKey();
-                if (result == '*') {
-                  break;    
-                } else if (result > 0 && result < 10) {
-                  //Custom att      
-                }
+              tone(buzzer,beep1,beep1len);
+              char* output = input("Heading, Pitch, Roll",',');
+              int outsize = strlen(output);
+              
+              Serial.print('[');
+              Serial.print('A');
+              Serial.print('3');
+              for (int x = 0; x < outsize; x++) {
+                Serial.write(output[x]);
               }
+              Serial.print(']');
+              write_menu(menu1,0);
               break;  
             } else if (result == '4') {
               //Off
+              Serial.print("[A4]");
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("Attitude control off");
+              ak_tone();
+              delay(1000);
+              write_menu(menu1,0);
+              break;
             }
           }    
         }
       }
-      
     } else if (result == '2') {
-      write_menu(menu2);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Loading scripts...");
+      Serial.print("[R2]");
       while (1) {
-        
-        result = keypad.getKey(); 
+        if (Serial.available() > 0) {
+          //Wait until end character.  
+        }
+         
       }
-       
+      //Split data into list of script names
+      
+      
+      //Have mini menu program with pages if #ofscripts > 4
+      
+      
+      
     } else if (result == '3') {
-      write_menu(menu3);
+      write_menu(menu3,1);
       while (1) {
         
         result = keypad.getKey();
         if (result == '*') {
-          write_menu(menu0);
+          write_menu(menu0, 1);
           break;  
         } else if (result == '1') {
           //Type in telemachus server
+          tone(buzzer,beep1,beep1len);
+          char *output = input("Enter server IP:", '.');
+          int outsize = strlen(output);
+          
+          Serial.print('[');
+          Serial.print('I');
+          Serial.print('1');
+          for (int x = 0; x < outsize; x++) {
+            Serial.print(output[x]);
+          }
+          Serial.print(']');
+          write_menu(menu3,0);
+          
         } else if (result == '2') {
-          //Type in multiplayer server  
+          //Multiplayer server
+          tone(buzzer,beep1,beep1len);
+          char *output = input("Enter server IP:", '.');
+          int outsize = strlen(output);
+          
+          Serial.print('[');
+          Serial.print('I');
+          Serial.print('2');
+          for (int x = 0; x < outsize; x++) {
+            Serial.print(output[x]);
+          }
+          Serial.print(']');
+          write_menu(menu3,0);
+          
         } else if (result == '3') {
-          //Enable Multiplayer 
+          //Enable Multiplayer
+          
         } else if (result == '4') {
-          //Nothing yet  
+          //Wifi preset
+          
         }
         
       } 
     } else if (result == '4') {
-      Serial.println("Shutting down...");
-      Serial1.print("[H]");
+      Serial.print("[H]");
+      
+      for (int x = 0; x < 20; x++) {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Please wait ");
+        lcd.print(20 - x);
+        lcd.setCursor(0,1);
+        lcd.print("seconds until Raspi");
+        lcd.setCursor(0,2);
+        lcd.print("fully shuts down");
+        delay(1000);
+        
+      }
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("It is now safe to");
+      lcd.setCursor(0,1);
+      lcd.print("turn off power.");
+      exit(0);
+      
     }
   } 
 }
